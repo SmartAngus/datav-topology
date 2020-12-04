fd68220cda44f0f2433612cbd188cb6a5747a06c
AnnDon <majy89@126.com>
2020-12-04 17:39:32 +0800

    重写实时曲线配置

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -11,7 +11,7 @@ import preBgImg2 from './bg02.jpg';
 import preBgImg3 from './bg03.jpg';
 
 const { confirm } = Modal;
-const token = '3LPNqmD3zH3JbnSb9yJH0m';
+const token = '2IIWVcdnL4d6Pf51ozwfvS';
 const testId="e2e697e719194b188c1bfe5b0dcb0604"
 
 const EditorLayoutCanvas: React.FC<any> = ({ ...props }) => {
